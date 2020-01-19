dc85f7d2432b46e5e35795bf37305464bc256ea9
root <root@IncredibleSRV.localdomain>
2020-01-19 19:29:15 +0530

    config server added

---
+++ ConfigServer/src/main/java/com/srv/tsukinome/TsukinomeApplication.java
@@ -0,0 +1,13 @@
+package com.srv.tsukinome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsukinomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsukinomeApplication.class, args);
+    }
+
+}
